4e4600717b727bc6a10682d8372464eaf0da37eb
dharavath_ramdas <dharavathramdas101@gmail.com>
2022-12-20 15:10:56 +0530

    updated the requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import find_packages,setup
 
+
+def get_requirements():
+    pass
+
 setup(
     name="sensor",
     version="0.01",
-    author="ineuron"
+    author="ineuron",
+    author_email="dharavathdas101@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
 )
